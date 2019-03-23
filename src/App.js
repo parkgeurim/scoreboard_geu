faf9a224ca81fd5cde4b553c970af10e18f72596
thedodum <jeonyk@neoforth.com>
2019-03-23 15:58:48 +0900

    페이지 방식에서 모듈 방식으로 카피

--- src/App.js
+++ src/App.js
@@ -1,57 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Header} from './components/Header';
+import {Player} from "./components/Player";
 
-const Header =(props) => {
-  console.log(props);
-  const {title, totalPlayers} = props; // destruct assingments
-  return (
-    <header>
-      <h1>{title}</h1>
-      <span className="stats">Players: {totalPlayers}</span>
-    </header>
-  );
-};
 
-class Counter extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state= {
-      score : 30,
-      name : 'a'
-    }
-  }
-
-  incrementScore = () => {
-    this.setState(prevState => {
-      return {
-        score: prevState.score+1
-      }
-    });
-  }
-
-  render(){
-    return (
-      <div className="counter">
-        <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
-      </div>
-    );
-  }
-}
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button className="remove-player" onClick={() => props.handleRemovePlayer(props.id)}>X</button>
-      </span>
-      <span className="player-name">{props.name}</span>
-      <Counter />
-    </div>
-  );
-};
 class App extends React.Component {
 
   state = {
